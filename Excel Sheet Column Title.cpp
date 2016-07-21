673c4687d9588ebc3b9ee9ee9330be2fb18c8aa1
awplxz <786764008@qq.com>
2016-07-21 09:43:42 +0800

    Update Excel Sheet Column Title.cpp

--- Excel Sheet Column Title.cpp
+++ Excel Sheet Column Title.cpp
@@ -3,6 +3,6 @@ public:
     string convertToTitle(int n) {
         if(n<=0)
             return "";
-        return convertToTitle(n / 26) + (char)(--n % 26 + 'A');
+        return convertToTitle((n-1) / 26) + (char)(--n % 26 + 'A');
     }
 };
